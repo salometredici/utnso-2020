654c493a435b40eb965d973c77b387fc7e05f2dd
utnso <salome.tredici@outlook.com>
2020-10-13 02:29:56 -0300

    Added bitmap initialization

--- sindicato/include/sindicato_init.h
+++ sindicato/include/sindicato_init.h
@@ -1,26 +1,43 @@
 #ifndef SINDICATO_INIT_H
 #define SINDICATO_INIT_H
 
+#include <sys/mman.h>
 #include <commons/bitarray.h>
 #include "../../shared/include/shared_commons.h"
 #include "../../shared/include/shared_core.h"
 #include "../../shared/include/shared_serialization.h"
 #include "tests.h"
 
-#define METADATA_PATH "/Metadata/Metadata.AFIP"
-#define BITMAP_PATH "/Metadata/Bitmap.bin"
-#define BLOCKS_PATH "/Blocks/"
 #define FILES_PATH "/Files/"
-#define RESTAURANTES_PATH "/Files/Restaurantes/"
+#define BLOCKS_PATH "/Blocks/"
+#define BITMAP_PATH "/Metadata/Bitmap.bin"
+#define METADATA_PATH "/Metadata/Metadata.AFIP"
 #define RECETAS_PATH "/Files/Recetas/"
+#define RESTAURANTES_PATH "/Files/Restaurantes/"
+
+int puertoEscucha;
 
 char *puntoMontaje;
 char *dirInicial;
 char *blocksPath;
 char *filesPath;
 
-t_config *metadata;
+int blockSize;
+int blocksQuantity;
 
-t_bitarray *bitMap;
+t_bitarray *bitarray;
+
+pthread_mutex_t mx1;
+pthread_mutex_t mx2;
+
+void init();
+
+// crear archivo
+// borrar archivo
+// validar archivo
+// obtener data
+// guardar data
+// borrar data
+// actualizar data
 
 #endif
